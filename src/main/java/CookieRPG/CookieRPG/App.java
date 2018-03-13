12d3f28e70317b016379edb507e3a4efbaa84b4b
Maximilian Kling <kling-maximilian@jectrum.de>
2018-03-13 22:24:27 +0100

    Barebone Upload

---
+++ src/main/java/CookieRPG/CookieRPG/App.java
@@ -0,0 +1,13 @@
+package CookieRPG.CookieRPG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
